0776947ff33a56b3ea80ba87c944d3cc2fe9fa3d
Mohamed Ramadan <m7mdrama1245@gmail.com>
2021-04-10 20:43:16 +0200

    updating query cache after mutation

--- resources/js/components/AddCardBottom.vue
+++ resources/js/components/AddCardBottom.vue
@@ -4,6 +4,7 @@
 
 <script>
 import CardAdd from './../graphql/CardAdd.gql'
+import BoardQuery from './../graphql/BoardWithListsAndCards.gql'
 
 export default {
    methods: {
@@ -14,6 +15,14 @@ export default {
                    title: "Added through mutation",
                    listId: 1,
                    order:1
+               },
+               update(store, {data: {cardAdd} }) {
+                   const data = store.readQuery({
+                       query: BoardQuery,
+                       variables: {id: 1}
+                   });
+                   data.board.lists.find(list => (list.id = 1)).cards.push(cardAdd);
+                   store.writeQuery({ query: BoardQuery, data})
                }
            })
        }
